814b306f10100f44cafd9352e191e739e71d151a
ebolzan@inf.ufsm.br <ebolzan@inf.ufsm.br@a7f994dc-fb6d-e0ff-e20b-ca6275f53434>
2013-11-02 20:32:30 +0000

    cadatro de usuarios

--- src/java/model/User.java
+++ src/java/model/User.java
@@ -14,21 +14,27 @@ public class User {
     protected String email;
     protected String endereco;
 
-    public User(String nome, String email, String endereco) {
-        this.nome = nome;
-        this.email = email;
-        this.endereco = endereco;
-    }   
-
     public String getNome() {
         return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getEndereco() {
         return endereco;
-    }    
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }      
 }
